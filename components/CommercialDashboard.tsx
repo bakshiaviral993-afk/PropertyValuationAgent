128bcff9139bb2b2778e9c83384a6f75d38c680a
bakshiaviral993-afk <bakshiaviral993@gmail.com>
2026-01-19 15:49:14 +0530

    Update CommercialDashboard.tsx

--- components/CommercialDashboard.tsx
+++ components/CommercialDashboard.tsx
@@ -18,16 +18,7 @@ import React, { useState } from 'react';
 import { LayoutGrid, FileText } from 'lucide-react';
 import ValuationReport from './ValuationReport';
 // Add tab button and view:
-{viewMode === 'report' && (
-  <ValuationReport 
-    mode="buy" // or 'rent', 'land', 'commercial'
-    result={result}
-    city={city}
-    area={area}
-    pincode={pincode}
-    userInput={userInput}
-  />
-)}
+
 
 interface CommercialDashboardProps {
   result: CommercialResult;
